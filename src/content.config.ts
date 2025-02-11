fa34945720a03e8aef5ecd4d54cdebb4513b02f5
Julian Mills <julian.exodius@gmail.com>
2025-02-11 22:45:49 +0100

    fmt

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
 	loader: glob({ pattern: "*.md", base: "./content/blog" }),
@@ -7,7 +7,7 @@ const blog = defineCollection({
 		title: z.string(),
 		enabled: z.boolean(),
 		authors: z.array(z.string()),
-	})
+	}),
 });
 
 export const collections = { blog };
