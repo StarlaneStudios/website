dfc1d2ab344420eaff96263b9ca35e56794e35c2
ItsMateo <me@itsmateo.dev>
2025-02-14 20:17:08 -0500

    feat: finish blogs

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { BlogSchema } from "./schemas";
 
-const blogs = defineCollection({
+export const blogs = defineCollection({
 	loader: glob({ pattern: "*.mdx", base: "./content/blogs" }),
 	schema: BlogSchema,
 });
